03665f17caca19d871fd495dd2ae398c38ce87a4
gnd-vn <victor.hugo.negosek.de.oliveira@ipt.fraunhofer.de>
2022-04-11 10:05:55 +0200

    Code

---
+++ MQTTPublisher/Publisher.cs
@@ -0,0 +1,56 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace MQTTPublisher
+{
+    class PubliSubssher
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                            .WithClientId(Guid.NewGuid().ToString())
+                            .WithTcpServer("test.mosquitto.org", 1883)
+                            .WithCleanSession()
+                            .Build();
+
+            client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("Connected to the broker sucessfully");
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from the broker successfully");
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.WriteLine("Please press a key to publish the message");
+
+            Console.ReadLine();
+
+            await PublishMessageAsync(client);
+
+            await client.DisconnectAsync();
+        }
+
+        private static async Task PublishMessageAsync(IMqttClient client)
+        {
+            string messagePayload = "Hello!";
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("RishabhSharma")
+                .WithPayload(messagePayload)
+                .WithAtLeastOnceQoS()
+                .Build();
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+            }
+        }
+    }
+}
---
+++ MQTTSubscriber/Subscriber.cs
@@ -0,0 +1,50 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTTSubscriber
+{
+    class Subscriber
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                            .WithClientId("ConnectedMachineClient")
+                            .WithTcpServer("mqtt.dpart.svc.fortknox.local", 1883)
+                            .WithCleanSession()
+                            .Build();
+
+
+            client.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Connected to the broker sucessfully");
+                var topicFilter = new TopicFilterBuilder()
+                                    .WithTopic("$SYS/broker/uptime")
+                                    .Build();
+                await client.SubscribeAsync(topicFilter);
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from the broker successfully");
+            });
+
+            client.UseApplicationMessageReceivedHandler(e =>
+            {
+                var message = ($"Received Message - {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine(message);
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+        }
+    }
+}
